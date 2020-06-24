12fe1272b9fdf2d951c1990ba442ed16ceda422f
Kyle <kt@wolf-tek.com>
2020-06-24 13:33:06 -0500

    should fix pages

---
+++ build/precache-manifest.65760b61833dc65dc374ef74eb2d2481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5400698aa3267e2710e636a7cd8a0abf",
+    "url": "/breefout/Quiz-Website/index.html"
+  },
+  {
+    "revision": "3d25f7ddc871cbe2f93d",
+    "url": "/breefout/Quiz-Website/static/css/main.16f4f4c8.chunk.css"
+  },
+  {
+    "revision": "0937e22b3aa431e0e89b",
+    "url": "/breefout/Quiz-Website/static/js/2.3328f6a7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/breefout/Quiz-Website/static/js/2.3328f6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d25f7ddc871cbe2f93d",
+    "url": "/breefout/Quiz-Website/static/js/main.e986b8db.chunk.js"
+  },
+  {
+    "revision": "7a013409a1d3ed50534c",
+    "url": "/breefout/Quiz-Website/static/js/runtime-main.1455f1c5.js"
+  }
+]);
