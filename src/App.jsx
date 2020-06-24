e9c320cee69cf4bdb4298c64322fd3ebf5d0a852
Kyle <kt@wolf-tek.com>
2020-06-24 15:26:30 -0500

    final touches

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ class App extends React.Component {
 
     var answerArr = [];
     answerArr.length = howgay.questions.length;
-    answerArr.fill(0);
+    answerArr.fill(undefined);
 
     this.state = {
-      answers: answerArr
+      answers: answerArr,
+      finalResult: null
     };
 
     this.updateScore = this.updateScore.bind(this);
@@ -28,6 +29,12 @@ class App extends React.Component {
   }
 
   calculateResult() {
+    let unanswered = this.state.answers.indexOf(undefined);
+    if (unanswered > -1) {
+      alert("Question #" + (unanswered + 1) + " needs to be answered.");
+      return;
+    }
+
     var totalScore = 0;
     var totalP = 0;
     var totalA = 0;
@@ -44,11 +51,42 @@ class App extends React.Component {
       }
     });
 
-    alert(totalScore);
+    if (totalScore <= 0 && totalA > howgay.extras.asexual.minimumScore) {
+      this.setState({finalResult: [howgay.responses[0]]});
+      return;
+    }
+
+    var endResponse = howgay.responses.find((response) => response.minimumScore <= totalScore && totalScore <= response.maximumScore);
+    console.log(endResponse);
+    if (endResponse == undefined) {
+      if (totalScore < 0) {
+        endResponse = howgay.responses[1];
+      } else if (totalScore > howgay.responses[howgay.responses.length-1].maximumScore) {
+        endResponse = howgay.responses[howgay.responses.length-1];
+      }
+    }
+    console.log(endResponse);
+
+    this.setState({finalResult: [endResponse, totalP > howgay.extras.pansexual.minimumScore]});
   }
 
   render() {
-    return <div id="background">
+    if (this.state.finalResult) {
+      let result = this.state.finalResult;
+      return <div id="background">
+        <Card id="endCard">
+          <Card.Title>{"You are " + result[0].title}</Card.Title>
+          <Card.Body>
+            <div style={{marginBottom:"20px"}}>{result[0].description}</div>
+          {
+            result[1] ? <div style={{marginBottom:"20px"}}>{howgay.extras.pansexual.title}</div> : null
+          }
+          <Button style={{width:"80px"}} onClick={() => this.setState({finalResult: null})} variant="primary">Okay</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    } else {
+      return <div id="background">
       <Card style={{margin:"20px"}}>
         <h1 id="quizTitle">{howgay.quizTitle}</h1>
       </Card>
@@ -63,6 +101,7 @@ class App extends React.Component {
         </Card>)}
         <Button onClick={this.calculateResult} variant="primary" style={{marginLeft:"20px"}}>Submit</Button>
     </div>
+    }
   }
 }
 
